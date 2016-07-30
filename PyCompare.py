aec2ddc65416c05c9676f32207e72102771bbc4b
David Davó <david@ddavo.me>
2016-07-30 02:39:40 +0200

    Added mail support and PyCompare

---
+++ PyCompare.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#fdupes
+import subprocess, sys, time
+tmp = subprocess.Popen(["fdupes", "/home/davo/Imágenes/Wallpapers/", "-rS"], stdout=subprocess.PIPE)
+tmp = str(tmp.stdout.read().decode("utf-8"))
+print(tmp)
+print(int((len(tmp.split("\n"))-1)/4))
+
+if "--mail" in sys.argv and len(tmp.split("\n")) > 2:
+    import smtplib
+
+    from email.message import EmailMessage
+    from email.headerregistry import Address
+    from email.utils import make_msgid
+
+    sender = "david@ddavo.me"
+    receiv = "david@ddavo.me"
+    msg = EmailMessage()
+    msg["From"] = Address("Davo-Arch10", "david@ddavo.me")
+    msg["To"] = Address("Yo mismo", "david@ddavo.me")
+    msg['Subject'] = "Wallpapers compared " + time.strftime("%d/%m/%Y %H:%M:%S")
+    msg.preamble = "WTF"
+    contenido = "Duplicados encontrados {}:\n\n".format(int((len(tmp.split("\n"))-1)/4)) + tmp
+    msg.set_content(contenido)
+
+    try:
+        smtpObj = smtplib.SMTP('smtp.ddavo.me')
+        smtpObj.send_message(msg)
+        print("Successfully sent email")
+        smtpObj.quit()
+    except:
+        print("Unable to to send email")
+        raise
--- PySortbck.py
+++ PySortbck.py
@@ -1,15 +1,22 @@
-import sys, os, subprocess
+import sys, os, subprocess,time
 import random
+import smtplib
 from shutil import move
+from email.message import EmailMessage
+from email.headerregistry import Address
+from email.utils import make_msgid
 
 source = "/home/davo/Imágenes/Wallpapers/"
 print("Source:", source)
 files = os.listdir(source)
 os.chdir(source)
+wallsorted = []
 
 def main(ratio, destination):
-    global source
     global files
+    global source
+    
+    files = os.listdir(source)
     #print("R,d", ratio, destination)
     if not os.path.exists(destination):
         print("No existia la carpeta, la hemos tenido que crear")
@@ -17,16 +24,21 @@ def main(ratio, destination):
     print("Destination", destination)
     for f in files:
         if f.endswith(".png") or f.endswith(".jpg"):
-            out = subprocess.check_output('identify -format "%[fx:abs((' + str(ratio) + ')-(w/h))]:%M\n" ' + f + ' | sort -n -k1 -t:', shell=True).decode("utf-8").replace("\n", "").split(":")
-            #print(out[0] + ":" + out[1])
-            if float(out[0]) < 0.15:
-                try:
-                    move(source + out[1],destination + out[1])
-                    print("Moviendo archivo", out[1])
-                    files.remove(f)
-                except:
-                    print("No se ha podido copiar", out[1])
-                    raise
+            try:
+                out = subprocess.check_output('identify -format "%[fx:abs((' + str(ratio) + ')-(w/h))]:%M\n" ' + f + ' | sort -n -k1 -t:', shell=True).decode("utf-8").replace("\n", "").split(":")
+                #print(out[0] + ":" + out[1])
+                if float(out[0]) < 0.15:
+                    try:
+                        move(source + out[1],destination + out[1])
+                        print("Moviendo archivo", out[1])
+                        wallsorted.append("Movido " + source + out[1] + " a " + destination + out[1])
+                        files.remove(f)
+                    except:
+                        print("No se ha podido copiar", out[1])
+                        wallsorted.append("No se ha podido copiar " + out[1])
+                        raise
+            except:
+                print("Error con {}".format(f))
         else:
             #print(f, "no termina en png, jpg")
             pass
@@ -35,3 +47,25 @@ main(16/9, "16x9/")
 main(16/10, "16x9/")
 main(9/16, "Vertical/")
 main(4/3, "4x3/")
+
+if "--mail" in sys.argv and len(wallsorted) > 0:
+    sender = "david@ddavo.me"
+    receiv = "david@ddavo.me"
+    msg = EmailMessage()
+    msg["From"] = Address("Davo-Arch10", "david@ddavo.me")
+    msg["To"] = Address("Yo mismo", "david@ddavo.me")
+    msg['Subject'] = "Wallpapers sorted " + time.strftime("%d/%m/%Y %H:%M:%S")
+    msg.preamble = "WTF"
+    contenido = "\n".join(wallsorted)
+    msg.set_content(contenido)
+
+    #message = msg.as_string()
+
+    try:
+        smtpObj = smtplib.SMTP('smtp.ddavo.me')
+        smtpObj.send_message(msg)
+        print("Successfully sent email")
+        smtpObj.quit()
+    except:
+        print("Unable to to send email")
+        raise
