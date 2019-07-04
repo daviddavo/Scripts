f003568071c63026a9186bd25115a29a269390a9
David Davó <david@ddavo.me>
2019-07-04 13:35:28 +0200

    Sorting backgrounds improved

--- PySortbck.py
+++ PySortbck.py
@@ -8,38 +8,54 @@ CONFIG_DIR   = "Config.ini"
 DEBUG = True
 MORELESS = .10
 UNKNOWN_FOLDER = "UnknownResolution"
+ERROR_FOLDER = "Error"
+TOO_LOW_FOLDER = "TooLowResolution"
 # UNKNOWN_FOLDER = None
-tosort = [(16/9,"16x9/"), (9/16,"Vertical/"), (4/3,"4x3/")]
+tosort = [(16/9,"16x9/", 1920, 1080), (9/16,"Vertical/", 1080, 1920), (4/3,"4x3/", 1400, 1050)]
 
 def getToMove(toMove, fname):
     files = os.listdir(fname)
     for f in files:
         af = os.path.join(fname, f)
 
-        mime = mimetypes.guess_type(af)[0]
-        if mime is not None and mime.split("/")[0] == "image":
-            im = Image.open(af)
-            w, h = im.size
-            
-            for r in tosort:
+        try:
+            mime = mimetypes.guess_type(af)[0]
+            if mime is not None and mime.split("/")[0] == "image":
+                im = Image.open(af)
+                w, h = im.size
+                
                 sort = False
-                if (r[0]-MORELESS < w/h < r[0]+MORELESS):
-                    toMove[r[1]].append(f)
-                    print("Moving %s to %s" % (f, r[1]))
-                    sort = True
-                    break
+                for r in tosort:
+                    if (r[0]-MORELESS < w/h < r[0]+MORELESS):
+                        if (TOO_LOW_FOLDER is not None and (w < r[2] or h < r[3])):
+                            toMove[TOO_LOW_FOLDER].append(f)
+                        else:
+                            toMove[r[1]].append(f)
+                        
+                        print("Moving %s to %s" % (f, r[1]))
+                        sort = True
+                        break
 
-            if UNKNOWN_FOLDER is not None and not sort:
-                toMove[UNKNOWN_FOLDER].append(f)
+                if UNKNOWN_FOLDER is not None and not sort:
+                    toMove[UNKNOWN_FOLDER].append(f)
+        except:
+            if (ERROR_FOLDER is not None): toMove[ERROR_FOLDER].append(f)
 
 def processFolder(fname):
-
     toMove = {}
-    if UNKNOWN_FOLDER is not None:
-        if not os.path.exists(os.path.join(fname, UNKNOWN_FOLDER)):
-            print("Creating folder %s" % fname)
-            os.makedirs(os.path.join(fname, UNKNOWN_FOLDER))
-        toMove[UNKNOWN_FOLDER] = []
+
+    def auxCreateFolder(folder):
+        if folder is not None:
+            if not os.path.exists(os.path.join(fname, folder)):
+                print("Creating folder %s" % folder)
+                os.makedirs(os.path.join(fname, folder))
+
+            toMove[folder] = []
+
+
+    auxCreateFolder(UNKNOWN_FOLDER)
+    auxCreateFolder(TOO_LOW_FOLDER)
+    auxCreateFolder(ERROR_FOLDER)
 
     # First we create needed folders
     for r in tosort:
