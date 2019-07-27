c0e87224cbaa0ae0b11803f87f6da58129ced4a0
David Davó <david@ddavo.me>
2019-07-27 17:24:28 +0200

    PyBackground

--- PyBackground.py
+++ PyBackground.py
@@ -1,5 +1,6 @@
 import os, sys
 import threading
+import configparser
 import sqlite3
 import random
 from gi.repository import GLib
@@ -9,9 +10,14 @@ from Xlib.ext import randr
 
 MORELESS = .10
 
-ImgFolder = os.path.join(GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_PICTURES), "Wallpapers/")
-# ImgFolder = "$XDG_PICTURES_DIR/Wallpapers/"
-resolutions = [(16/9,"16x9/"), (9/16,"Vertical/"), (4/3,"4x3/")]
+CONFIG_DIR = "/home/davo/Scripts/Config.ini"
+
+config = configparser.RawConfigParser()
+config.read(CONFIG_DIR)
+
+ImgFolder = config.get("BACKGROUNDS", "background-folder")
+if (config.getboolean("BACKGROUNDS", "background-folder-auto")):
+    ImgFolder = os.path.join(GLib.get_user_special_dir(GLib.UserDirectory.DIRECTORY_PICTURES), "Wallpapers/")
 
 # TODO: Load this from config
 screen_arr = [
@@ -40,26 +46,13 @@ def thr_set_background(head, fname, howchanged):
 #feh --bg-fill /Directorio/Imagen.png
 #Nota, copia .fehbg a esta carpeta
 def main():
-    '''
-    d = display.Display() # default display
-    s = d.screen()
-    print(d.screen_count())
-
-    for output in randr.get_screen_resources(s.root).outputs:
-        d.get_output_info(output)
-        print(str(randr.list_output_properties(s.root, output)))
-
-    for m in get_monitors():
-        w, h = m.width, m.height
-        print(m.name)
-        # if (r[0]-MORELESS < w/h < r[0]+MORELESS):
-    '''     
-
     howchanged = sys.argv[1] if len(sys.argv)>1 else "unknown"
 
-    for i, screen in enumerate(screen_arr):
-        t = threading.Thread(target=thr_set_background, args=(i, screen["folder"], howchanged))
+    for i in range(config.getint("BACKGROUNDS", "number-screens")):
+        fname = os.path.join(ImgFolder, config.get("BACKGROUNDS", "screen-"+str(i)+"-folder"))
+        t = threading.Thread(target=thr_set_background, args=(i, fname, howchanged))
         t.start()
+        
 
 if __name__ == "__main__":
     main()
