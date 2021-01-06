4e45388d548af3f93fd3e554602d505a0f2ef268
David Davó <david@ddavo.me>
2021-01-06 10:01:02 +0100

    beetall with if

--- beetall.sh
+++ beetall.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-~/Scripts/OnPlaylist.sh
-beet alt update android
-beet mbsync
-beet up
-beet lastgenre
-beet fetchart
-beet extractart
-beet acousticbrainz
-beet write
+
+emptyalbums=$(beet ls -a mb_albumid:'^$')
+if [ -z $emptyalbums ]; then
+    ~/Scripts/OnPlaylist.sh
+    beet alt update android
+    beet mbsync
+    beet up
+    beet lastgenre
+    beet fetchart
+    beet extractart
+    beet acousticbrainz
+    beet write
+else
+    echo "There are the following albums without mb_albumid" >2
+    echo $emptyalbums > 2
+fi
